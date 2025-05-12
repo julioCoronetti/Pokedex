8d9e36a51ee3d54e16692fd8e13886fb9d6e77f9
Julio Coronetti <timeset13@Outlook.com>
2025-05-12 20:55:32 -0300

    feat: implement Pokémon fetching and display in Pokedex component

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
 export const Pokedex = () => {
+	const [pokemons, setPokemons] = useState([] as any);
+
+	const fetchPokemons = async () => {
+		const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+		const data = await response.json();
+		setPokemons(data.results);
+	};
+
+	useEffect(() => {
+		fetchPokemons();
+	}, []);
+
 	return (
 		<div>
 			<h1 className="text-3xl mt-13 mb-5">Pokédex</h1>
+			<ul>
+				{pokemons.map((pokemon: any, index: number) => (
+					<li key={index}>
+						{pokemon.name}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
