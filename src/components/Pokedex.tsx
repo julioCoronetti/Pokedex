8eedf0bbb71162b6e5015e2b433e3e091e1884a7
Julio Coronetti <timeset13@Outlook.com>
2025-05-12 20:47:07 -0300

    refactor: simplify components by removing unused state and effects

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,27 +1,7 @@
-import { useState, useEffect } from "react";
-import { PokemonCard } from "./PokemonCard";
-
 export const Pokedex = () => {
-	const [pokemons, setPokemons] = useState([]);
-
-	async function fetchPokemon() {
-		const response = await fetch("https://pokedex.mimo.dev/api/pokemon");
-		const data = await response.json();
-		setPokemons(data);
-	}
-
-	useEffect(() => {
-		fetchPokemon();
-	}, []);
-
 	return (
-		<>
-			<h1>Pokédex</h1>
-			<ul>
-				{pokemons.map((pokemon, index) => (
-					<PokemonCard key={index} pokemon={pokemon} />
-				))}
-			</ul>
-		</>
+		<div>
+			<h1 className="text-3xl mt-13 mb-5">Pokédex</h1>
+		</div>
 	);
 };
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	theme: {
-		extend: {
-			fontFamily: {
-				press: ['"Press Start 2P"', "cursive"],
-			},
-		},
-	},
-};
