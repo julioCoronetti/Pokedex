930a88eb0ef14533ba4305541213d85e4cdd2044
Julio Coronetti <timeset13@Outlook.com>
2025-05-13 19:21:49 -0300

    feat: refactor Search component to improve Pokémon fetching and filtering logic

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,50 +1,59 @@
 import { useEffect, useState } from "react";
-import type { PokemonProps } from "./Pokedex";
 import { PokemonCard } from "./PokemonCard";
+import type { PokemonProps } from "./Pokedex";
 
 export const Search = () => {
-	const [pokemons, setPokemons] = useState<PokemonProps[]>([]);
-	const [filteredPokemons, setFilteredPokemons] = useState<PokemonProps[]>([]);
+	const [allPokemons, setAllPokemons] = useState<PokemonProps[]>([]);
+	const [filteredPokemons, setFilteredPokemons] = useState<any[]>([]);
 	const [input, setInput] = useState("");
 
-	const fetchPokemons = async () => {
-		const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
-		const data = await response.json();
-
-		setPokemons(data.results);
-	};
-
 	useEffect(() => {
-		fetchPokemons();
+		fetch("https://pokeapi.co/api/v2/pokemon?limit=1302")
+			.then((res) => res.json())
+			.then((data) => setAllPokemons(data.results));
 	}, []);
 
 	useEffect(() => {
-		if (input === "") {
-			setFilteredPokemons([]);
-		} else {
-			const filtered = pokemons.filter((pokemon) => {
-				return pokemon.name.toLowerCase().includes(input.toLowerCase());
-			});
-			setFilteredPokemons(filtered);
-		}
-	}, [input, pokemons]);
+		const fetchFiltered = async () => {
+			if (input === "") {
+				setFilteredPokemons([]);
+				return;
+			}
+
+			const filtered = allPokemons.filter((pokemon) =>
+				pokemon.name.toLowerCase().includes(input.toLowerCase()),
+			);
+
+			const detailed = await Promise.all(
+				filtered.slice(0, 10).map(async (pokemon) => {
+					const res = await fetch(pokemon.url);
+					return await res.json();
+				}),
+			);
+
+			setFilteredPokemons(detailed);
+		};
+
+		fetchFiltered();
+	}, [input, allPokemons]);
 
 	return (
 		<div>
 			<h1 className="text-3xl mt-13 mb-5">Search</h1>
 			<input
 				onChange={(e) => setInput(e.target.value)}
-				className="bg-amber-50"
+				className="bg-amber-50 p-2 rounded-md mb-5 outline-none"
+				placeholder="Search Pokémon..."
 			/>
-			<ul>
+			<ul className="grid grid-cols-3 gap-4 mb-13">
 				{filteredPokemons
 					.filter((pokemon) => pokemon.sprites?.front_default)
-					.map((pokemon, index) => (
+					.map((pokemon) => (
 						<PokemonCard
-							key={index}
+							key={pokemon.id}
 							name={pokemon.name}
 							sprites={pokemon.sprites}
-							url={pokemon.url}
+							url={`https://pokeapi.co/api/v2/pokemon/${pokemon.id}/`}
 						/>
 					))}
 			</ul>
