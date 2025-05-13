a7f6c678c678451f08f5b1582b388e66af8a3687
Julio Coronetti <timeset13@Outlook.com>
2025-05-12 22:45:54 -0300

    feat: implement Pokémon search functionality with fetching and filtering

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import type { PokemonProps } from "./Pokedex";
+import { PokemonCard } from "./PokemonCard";
+
 export const Search = () => {
-  return (
-	<div>
-		<h1 className="text-3xl mt-13 mb-5">Search</h1>
-	</div>
-  )
-}
+	const [pokemons, setPokemons] = useState<PokemonProps[]>([]);
+	const [filteredPokemons, setFilteredPokemons] = useState<PokemonProps[]>([]);
+	const [input, setInput] = useState("");
+
+	const fetchPokemons = async () => {
+		const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+		const data = await response.json();
+
+		setPokemons(data.results);
+	};
+
+	useEffect(() => {
+		fetchPokemons();
+	}, []);
+
+	useEffect(() => {
+		if (input === "") {
+			setFilteredPokemons([]);
+		} else {
+			const filtered = pokemons.filter((pokemon) => {
+				return pokemon.name.toLowerCase().includes(input.toLowerCase());
+			});
+			setFilteredPokemons(filtered);
+		}
+	}, [input, pokemons]);
+
+	return (
+		<div>
+			<h1 className="text-3xl mt-13 mb-5">Search</h1>
+			<input
+				onChange={(e) => setInput(e.target.value)}
+				className="bg-amber-50"
+			/>
+			<ul>
+				{filteredPokemons
+					.filter((pokemon) => pokemon.sprites?.front_default)
+					.map((pokemon, index) => (
+						<PokemonCard
+							key={index}
+							name={pokemon.name}
+							sprites={pokemon.sprites}
+							url={pokemon.url}
+						/>
+					))}
+			</ul>
+		</div>
+	);
+};
