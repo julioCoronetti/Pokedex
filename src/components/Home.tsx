52d754cfb372442aa2af9e9bd8187a21a3d75eef
julioCoronetti <timeset13@outlook.com>
2025-05-14 12:23:52 -0300

    feat: add Pokédex image and update Home component to display it

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,11 @@
+import pokedexImage from "../assets/pokedex.webp";
+
 export const Home = () => {
 	return (
-		<>
+		<div className="flex flex-col items-center">
 			<h1 className="text-3xl mt-13 mb-5">Welcome to Pokédex</h1>
 			<p>Explore the world of Pokémon with our comprehensive Pokédex.</p>
-		</>
+			<img className="w-90 mt-13" src={pokedexImage} />
+		</div>
 	);
 };
